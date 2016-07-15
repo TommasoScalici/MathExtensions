cf390c17f811239b8a1d44667313daf26aef1ed8
Tommaso Scalici <tommaso.scalici@outlook.it>
2016-07-15 11:29:22 +0200

    Added GlobalSuppressions and NuGet.nuproj

---
+++ LINQExtensions/GlobalSuppressions.cs
@@ -0,0 +1,3 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Potential Code Quality Issues", "RECS0130:'for' loop control variable is never modified", Justification = "We can live with it")]
